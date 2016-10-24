ec104e30c1c8d962629559b09e054893bc839915
fearmear <fearmear@gmail.com>
2016-10-25 01:34:26 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+console.log('hello, world!')
+
+// MANUAL (ATM):
+// Clear inventory
+// Arena
+// Kalima, Corps, Lab
+// Crystal Mine
+// Angel Temple
+// Chaos Castle
+// Illusion Temple
+// All Rewards
+// Elite Monsters hunt
+
+// TODO:
+// Blood Castle
+// Craft BC/DS tickets
+// Exchange
+// Devil Square
+// Free Epic Chest ?? Repeat
+// Free Pet ?? Repeat
+// EXP Dungeon
+// Zen Dungeon
+// Collect Arena Reward
+// Arena Cheer
+// Daily Quest (1h)
+// Target Quest (1h)
+// Buy potions task
+// Dissasemble task
+// Colosseum (30m + use teleport? + dissasemble loop)
+
+// Scheduling
+// Persistance
+
+// ADVANCED:
+// Angel Temple
+// Chaos Castle
+// Illusion Temple
+// Insufficient Potion Detection
+// Daily Quest Clear Detection
+// Target Quest Clear Detection
+// EXP Dungeon Clear Detection ?? Color
+// Kalima
+// Corps
+// Lab
+// Arena
+// Auto-clear inventory
+// Blood Castle Statue
+// Golden Forces
+// World Boss
+// Boss Castle?
+
+// Resizable window
